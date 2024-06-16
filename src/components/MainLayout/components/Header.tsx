ca5925ab2a4ab7f04e5178930f5de3f5897546e0
Rustam Satlykov <rustam.satlykov@proton.me>
2024-06-16 23:51:21 +0500

    feat: add new api path

--- src/components/MainLayout/components/Header.tsx
+++ src/components/MainLayout/components/Header.tsx
@@ -33,7 +33,7 @@ export default function Header() {
             underline="none"
             to="/"
           >
-            Satlykov Store!
+            Satlykov Store!!!!!!!
           </Link>
         </Typography>
 
